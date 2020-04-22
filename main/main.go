412f04ea7d092aec985638644a4306649f9c78ee
CHIRANTH <chiranthchandrashekar123@gmail.com>
2020-04-22 07:36:37 +0530

    update main.go

--- main/main.go
+++ main/main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	run "github.com/CHIRANTH-C/golang"
-	log "github.com/sirupsen/logrus"
+	run "github.com/CHIRANTH-C/gobasics"
 )
 
 func main() {
-	fmt.Println("Divya....")
-	//	run.Hello()
-	log.Info("INfo message...")
-	log.Warn("Warn message....")
-	log.Fatal("Fatal message....")
 	run.Hello()
 }
